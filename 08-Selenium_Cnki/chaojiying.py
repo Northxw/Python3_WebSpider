92229feea1d886b09780567435c6d03df22a6550
Northxw <northxw@163.com>
2018-11-04 23:49:14 +0800

    添加超级鹰打码平台

---
+++ 08-Selenium_Cnki/chaojiying.py
@@ -0,0 +1,53 @@
+# coding:utf-8
+
+import requests
+from hashlib import md5
+
+class Chaojiying(object):
+
+    def __init__(self, username, password, soft_id):
+        self.username = username
+        password = password.encode('utf8')
+        self.password = md5(password).hexdigest()
+        self.soft_id = soft_id
+        self.base_params = {
+            'user': self.username,
+            'pass2': self.password,
+            'softid': self.soft_id,
+        }
+        self.headers = {
+            'Connection': 'Keep-Alive',
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)',
+        }
+
+    def PostPic(self, im, codetype):
+        """
+        im: 图片字节
+        codetype: 题目类型 参考 http://www.chaojiying.com/price.html
+        """
+        params = {
+            'codetype': codetype,
+        }
+        params.update(self.base_params)
+        files = {'userfile': ('ccc.jpg', im)}
+        r = requests.post('http://upload.chaojiying.net/Upload/Processing.php', data=params, files=files, headers=self.headers)
+        return r.json()
+
+    def ReportError(self, im_id):
+        """
+        im_id:报错题目的图片ID
+        """
+        params = {
+            'id': im_id,
+        }
+        params.update(self.base_params)
+        r = requests.post('http://upload.chaojiying.net/Upload/ReportError.php', data=params, headers=self.headers)
+        return r.json()
+
+
+if __name__ == '__main__':
+    # 以下均为超级鹰平台提供的样码，没有修改.
+    chaojiying = Chaojiying('超级鹰用户名', '超级鹰用户名的密码', '96001')
+    im = open('a.jpg', 'rb').read()
+    print(chaojiying.PostPic(im, 1902))
+
