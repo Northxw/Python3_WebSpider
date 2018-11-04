5e42d48e9eaaa9adb87539576d9bb657a93dfe6b
Northxw <northxw@163.com>
2018-11-05 00:10:03 +0800

    update

--- 08-Selenium_Cnki/config.py
+++ 08-Selenium_Cnki/config.py
@@ -1,13 +1,17 @@
 # -*- coding:utf-8 -*-
 
-# 用户名
-USERNAME = 'Northxw008'
-
-# 密码
-PASSWORD = 'Your Password'
-
-# 邮箱
-EMAIL = 'Your Email'
+# 知网用户名
+USERNAME = 'Northxw0077'
+# 知网密码
+PASSWORD = 'wxf201678...'
+# 知网邮箱
+EMAIL = 'northxw@qq.com'
 
 # 目标站点
 URL = 'http://my.cnki.net/elibregister/commonRegister.aspx'
+
+# 超级鹰用户名、密码、软件ID、验证码类型
+CHAIJIYING_USERNAME = 'Northxw'
+CHAOJIYING_PASSWORD = 'wxf201678'
+CHAIJIYING_SOFT_ID = 897682
+CHAOJIYING_KIND = 1902
