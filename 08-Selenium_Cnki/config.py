1d85cf15f36197050eae115c577b3773b621bd6d
Northxw <northxw@163.com>
2018-11-04 16:41:50 +0800

    update

--- 08-Selenium_Cnki/config.py
+++ 08-Selenium_Cnki/config.py
@@ -1,13 +1,13 @@
 # -*- coding:utf-8 -*-
 
 # 用户名
-USERNAME = 'Northxw07'
+USERNAME = 'Northxw008'
 
 # 密码
-PASSWORD = 'wxf201678...'
+PASSWORD = 'Your Password'
 
 # 邮箱
-EMAIL = 'northxw@qq.com'
+EMAIL = 'Your Email'
 
 # 目标站点
 URL = 'http://my.cnki.net/elibregister/commonRegister.aspx'
