fed8cd10adbae351f9802823431cecf2b583f606
Northxw <northxw@163.com>
2018-11-07 08:11:16 +0800

    created utils

---
+++ 08-Selenium_Cnki/utils/config.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+# 知网用户名
+USERNAME = '要注册的用户名'
+# 知网密码
+PASSWORD = '要注册用的密码'
+# 知网邮箱
+EMAIL = 'northxw@qq.com'
+
+# 目标站点
+URL = 'http://my.cnki.net/elibregister/commonRegister.aspx'
+
+# 超级鹰用户名、密码、软件ID、验证码类型
+CHAIJIYING_USERNAME = 'Northxw'
+CHAOJIYING_PASSWORD = '**********'
+CHAIJIYING_SOFT_ID = ********
+CHAOJIYING_KIND = 1902  # 1902代表常见四位英文和数字组成的验证码。验证码类型可以在打码平台的题分表上查看
