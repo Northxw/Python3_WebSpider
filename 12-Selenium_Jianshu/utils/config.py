fea8061c1a95c167c7fde9c83208796f3983918a
Northxw <northxw@163.com>
2018-11-14 09:14:52 +0800

    upload

---
+++ 12-Selenium_Jianshu/utils/config.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+# 目标站点
+URL = 'https://www.jianshu.com/sign_in'
+
+# 账号、密码
+USER_PHONENUMBER = '13201723305'
+PASSWORD = 'wxf201678...'
+
+# 超级鹰用户名、密码、软件ID、验证码类型
+CHAIJIYING_USERNAME = 'Northxw'
+CHAOJIYING_PASSWORD = 'wxf201678'
+CHAIJIYING_SOFT_ID = 897682
+CHAOJIYING_KIND = 9004
