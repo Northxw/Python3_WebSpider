58c2eb70f26ffda6676216cd24daf7df9ae5ff5f
吴先森 <northxw@163.com>
2019-03-14 20:07:22 +0800

    Update Glory_of_Kings.py

--- 02-Golory_of_Kings/Glory_of_Kings.py
+++ 02-Golory_of_Kings/Glory_of_Kings.py
@@ -1,32 +1,35 @@
 # -*- coding:utf-8 -*-
 """
-Created on Mon Sep 17 21:27:00 2018
+Created at 21:27 at Sep 17,2018
 @author: Northxw
 """
+
 import requests
 import os
 
-# Get the list of all heroes
+# 全英雄列表请求链接
 herolist_url = 'https://pvp.qq.com/web201605/js/herolist.json'
-response = requests.get(herolist_url).json()    # Returns the JSON data that contains all the heroes.
+# 获取数据
+response = requests.get(herolist_url).json()
 
-# According to the hero's skin link, analyze and download the hero's skin.
-save_dir = "E:\Python\Spider\Ex\\01-Spider_Glory_of_Kings\hero_list\\"  # Specify download location
-if not os.path.exists(save_dir):    # Determine whether storage path exists, no creation
+# 根据英雄的皮肤链接，分析并下载英雄的皮肤
+save_dir = "E:\Python\Spider\Ex\\01-Spider_Glory_of_Kings\hero_list\\"  # 指定下载位置
+if not os.path.exists(save_dir):
     os.mkdir(save_dir)
 
 for i in range(len(response)):
-    # Get hero skin list
+    # 获取英雄皮肤列表
     skin_names = response[i]['skin_name'].split('|')
     for cnt in range(len(skin_names)):
-        # Download all skin of current hero
-        hero_num = response[i]['ename']     # hero num
-        hero_name = response[i]['cname']    # hero name
-        skin_name = skin_names[cnt]         # skin name
+        # 下载当前英雄的所有皮肤
+        hero_num = response[i]['ename']     # 英雄序号
+        hero_name = response[i]['cname']    # 英雄名称
+        skin_name = skin_names[cnt]         # 皮肤名称
 
-        save_file_name = save_dir + str(hero_num) + '-' + hero_name + '-' + skin_name + '.jpg'  # save_dir
+        save_file_name = save_dir + str(hero_num) + '-' + hero_name + '-' + skin_name + '.jpg'
         skin_url = 'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/{}/{}-bigskin-{}.jpg'.format(hero_num, hero_num, str(cnt+1))
-        response_skin_content = requests.get(skin_url).content      # Get the bit data of the picture
-
+        # 获取图片的位数据(二进制流数据)
+        response_skin_content = requests.get(skin_url).content
+        # 保存图片
         with open(save_file_name, 'wb') as f:
             f.write(response_skin_content)
